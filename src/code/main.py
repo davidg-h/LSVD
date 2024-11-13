82399c83e2f0c78459ff07dba3ea2db4a9a76d02
vnethrapalli <34689626+vnethrapalli@users.noreply.github.com>
2024-11-14 00:29:23 +0900

    added command line argument for prompt, added logging to main script

--- src/code/main.py
+++ src/code/main.py
@@ -1,15 +1,28 @@
 import os
+import sys
 from Text_to_Audio import Text2Audio
 from Computer_Vision import Text2Img, VidGen
 from Video_Audio import VidAudFusion
 
-prompt = "A happy woman walking down an alley with ice cream in her hand"
+
+# command line prompt entry
+prompt = ''
+if len(sys.argv) > 1:
+    prompt = sys.argv[1]
+
+# default prompt
+if prompt == '':
+    prompt = "A happy woman walking down an alley with ice cream in her hand"
 path = os.getcwd()
 
+print('generating audio...')
 _ , new_prompt = Text2Audio.text2audio(prompt, path)
 
+
+print('generating reference image...')
 Text2Img.text2img(new_prompt, path)
 
+print('generating video...')
 generator = VidGen.VideoGenerator(path, new_prompt)
 
 # generator.AnimateDiff()
@@ -21,5 +34,8 @@ audio = os.path.join(path, "musicgen_out.wav")
 video = os.path.join(path, "video file")
 output = os.path.join(path, "complete_out.mp4")
 
+print('merging audio and video...')
 VidAudFusion.install_ffmpeg()
 VidAudFusion.fuse_video_audio(video, audio, output)
+
+print(f'final advertisement found at: {output}')
